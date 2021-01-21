fa7e119985c8e34bff5ee1f847aeb1682f797249
deep.shah.7812@gmail.com <Sdeep@07812>
2021-01-21 15:46:10 -0500

    Lab-1 done with another change!

--- lab01/dml.sql
+++ lab01/dml.sql
@@ -1,7 +1,7 @@
 use csd220_lab1_deepshah;
 
 -- 1.	Change all windowless offices to utility rooms
-UPDATE room SET room_type = 'utility' WHERE room_type = 'office';
+UPDATE room SET room_type = 'utility' WHERE room_type = 'office' and room_has_window=0;
 SELECT * FROM room;
 
 -- 2.	Pick ONE of your buildings.  From ONLY this building, remove all bathrooms that have windows.
